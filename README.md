0106630e10c9c7463ab62370339b233d0a28ae12
Tim van den Dries <decks16@hotmail.com>
2020-05-02 04:21:55 +0200

    updated some small things. added jsfiddle-link to readme where you can see the script at work perfectly!

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Countdown_timer
-Countdown timer (HTML/jQuery/CSS) text + visual
+Countdown timer.
+
+Now fully javascript (no jQuery) multiple counters possible
+
+
+<br><br><br><br>
+
+Try it here:<br>
+https://jsfiddle.net/MrVamos/L817vg2o/12/
+
+<br>
+<br>
+<br>
+I will update this readme soon...
--- js/script.js
+++ js/script.js
@@ -135,18 +135,9 @@ function setTimer(distance) {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    if(hours < 10) {
-        hours = "0" + hours;
-    }
-
-    if(minutes < 10) {
-        minutes = "0" + minutes;
-    }
-
-    if(seconds < 10) {
-        seconds = "0" + seconds;
-    }
-
+    if(hours < 10) hours = "0" + hours;
+    if(minutes < 10) minutes = "0" + minutes;
+    if(seconds < 10) seconds = "0" + seconds;
     var timeLeft = hours + ":" + minutes + ":" + seconds;
     
     if(days !== 0) {
